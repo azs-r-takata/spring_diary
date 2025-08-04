b4e1c6618bee5c28ad1c958586b003ff901fbf63
azs-r-takata <r-takata@AZPC0175>
2025-08-04 13:18:04 +0900

    SpringBoot_オリジナル課題

--- src/main/java/com/example/diary/controller/DiaryController.java
+++ src/main/java/com/example/diary/controller/DiaryController.java
@@ -3,9 +3,12 @@ package com.example.diary.controller;
 import java.util.Collections;
 import java.util.List;
 
+import org.springframework.core.Conventions;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -23,67 +26,88 @@ public class DiaryController {
 		this.diaryService = diaryService;
 	}
 	
+	//ホーム画面
 	@GetMapping("/diary")
 	public String home(Model model) {
 		List<Diary> diaries = diaryService.getAllDiary();
 		Collections.reverse(diaries);
 		model.addAttribute("diaries", diaries);
 		
-		if(!model.containsAttribute("diaryForm")) {
-			model.addAttribute("diaryForm", new DiaryForm());
-		}
-		
 		return "diaryView";
-	}
+	} //End ホーム画面
 	
+	//詳細画面
 	@GetMapping("/diary/{id}")
 	public String detail(@PathVariable int id, Model model) {
 		model.addAttribute("diary", diaryService.getDiaryById(id).orElse(null));
 		return "diaryDetailsView";
-	}
+	} //End 詳細画面
 	
+	//新規投稿画面
 	@GetMapping("/diary/new")
-	public String Input() {
+	public String Input(Model model) {
+		if(!model.containsAttribute("diaryForm")) {
+			model.addAttribute("diaryForm", new DiaryForm());
+		}
 		return "diaryInputView";
-	}
+	} //End 新規投稿画面
 	
+	//編集画面
 	@GetMapping("/diary/edit/{id}")
 	public String editer(@PathVariable int id, Model model) {
 		model.addAttribute("diary", diaryService.getDiaryById(id).orElse(null));
+		
+		if(!model.containsAttribute("diaryForm")) {
+			model.addAttribute("diaryForm", new DiaryForm());
+		}
+		
 		return "diaryUpdateView";
-	}
+	} //End 編集画面
 	
+	//投稿保存処理
 	@PostMapping("/diary/save")
 	public String saveDiary(RedirectAttributes redirectAttributes,
-			DiaryForm form) {
-			//@RequestParam("diaryTitle") String diaryTitle,
-			//@RequestParam("diaryContent") String diaryContent) {
+			@Validated DiaryForm form, BindingResult result) {
+		if(result.hasErrors()) {
+			redirectAttributes.addFlashAttribute("diaryForm", form);
+			redirectAttributes.addFlashAttribute(BindingResult.MODEL_KEY_PREFIX + Conventions.getVariableName(form), result);
+			
+			return "redirect:/diary/new";
+		}
+		
 		try {
 			diaryService.inputDiary(form.getDiaryTitleForm(), form.getDiaryContentForm());
-			//diaryService.inputDiary(diaryTitle, diaryContent);
 			redirectAttributes.addFlashAttribute("successMessage", "日記の登録完了");
 		} catch (IllegalArgumentException e) {
 			redirectAttributes.addFlashAttribute("failureMessage", e.getMessage());
 		}
 		
 		return "redirect:/diary";
-	}
+	} //End 投稿保存処理
 	
+	//更新保存処理
 	@PostMapping("/diary/update")
 	public String updateDiary(RedirectAttributes redirectAttributes,
 			@RequestParam("diaryId") int diaryId,
-			@RequestParam("diaryTitle") String diaryTitle,
-			@RequestParam("diaryContent") String diaryContent) {
+			@Validated DiaryForm form, BindingResult result) {
+		if(result.hasErrors()) {
+			redirectAttributes.addFlashAttribute("diaryForm", form);
+			redirectAttributes.addFlashAttribute(BindingResult.MODEL_KEY_PREFIX + Conventions.getVariableName(form), result);
+			
+			return "redirect:/diary/edit/" + diaryId;
+		}
+		
 		try {
-			diaryService.updateDiary(diaryId, diaryTitle, diaryContent);
+			diaryService.updateDiary(diaryId, form.getDiaryTitleForm(), form.getDiaryContentForm());
 			redirectAttributes.addFlashAttribute("successMessage", "日記の更新完了");
 		} catch (IllegalArgumentException e) {
 			redirectAttributes.addFlashAttribute("failureMessage", e.getMessage());
 		}
 		
 		return "redirect:/diary";
-	}
+	} //End 更新保存処理
 	
+	//削除処理
 	@PostMapping("diary/delete/{id}")
 	public String deleteDiary(@PathVariable int id, RedirectAttributes redirectAttributes) {
 		try {
@@ -96,5 +120,5 @@ public class DiaryController {
 		}
 		
 		return "redirect:/diary";
-	}
+	} //End 削除処理
 }
--- src/main/java/com/example/diary/form/DiaryForm.java
+++ src/main/java/com/example/diary/form/DiaryForm.java
@@ -1,13 +1,15 @@
 package com.example.diary.form;
 
-import java.sql.Date;
-
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class DiaryForm {
-	private int diaryIdForm;
+	@NotBlank(message = "タイトルを入力してください。")
+	@Size(max = 10, message = "タイトルは10文字以内に収めてください。")
 	private String diaryTitleForm;
+	
+	@NotBlank(message = "本文を入力してください。")
 	private String diaryContentForm;
-	private Date diaryDateForm;
 }
